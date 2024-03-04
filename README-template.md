5ec90134750966663b2e4a5e0496b1a054af0049
Barbara <subwaynaas@yahoo.ie>
2024-03-04 19:31:02 +0200

    initial commit

---
+++ README-template.md
@@ -0,0 +1,5 @@
+This was a huge challenge for me. I first inserted the images on the cards as background images and placed them in the correct places BUT then when it came to the JS and I was appending a new section for the cards I realised I must insert them in my HTML rather than in my CSS. I had also used Flexbox on the cards but had to change them to Grid as it worked so much better when switching to a larger screen size.
+
+The JS was an enormous challenge and so I eventually watched a youtube video from Seer Studio and it was really good - I learned sooooo much! Thank you Seer Studio - you should make more videos!. This is the link to Seer Studio: https://www.youtube.com/watch?v=l9Qw8y3LfCY
+
+In the end it is not absolutely perfect as there is a very slight colored outline around the cards but maybe I will awake up in the middle of the night some time soon with the solution on how to fix it :) - ahh come on - we've all done that!!
---
+++ app.js
@@ -0,0 +1,134 @@
+//get all the btns daily, weekly and monthly and turn them into an array so that we can use the .click method on it later
+const btns = Array.from(document.querySelectorAll(".btns"));
+//console.log(btns);
+const container = document.querySelector("div.container");
+//console.log(container);
+
+let data = [];
+
+//this is to get the button that we are clicking on's option aka daily, weekly or monthly and then when we do to display the cards and the data from the json file on screen
+function clickButtons(item) {
+  item.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      activateClickedButton(btn);
+      const clickedOption = btn.dataset.option;
+      //console.log(clickedOption); // we get daily, weekly or monthly for which ever btn we click
+      //so that later when we displayCards we will grab the correct data from our dataArray which has a timeframe object with daily, weekly and monthly
+      displayCards(clickedOption);
+    });
+  });
+}
+
+//this is to get the button that we are clicking on to turn white etc re: the .active class
+function activateClickedButton(btn) {
+  btns.forEach((item) => {
+    //console.log(item);
+    //so loop thru all the btns and for each one if it is "active" remove it
+    item.classList.remove("active");
+    //then for the btn we are currently clicking - add the active class so that it will turn white
+    btn.classList.add("active");
+  });
+}
+
+//1st we fetch the data
+const loadData = async () => {
+  const response = await fetch("./data.json");
+  const fetchedData = await response.json();
+  data = fetchedData;
+  //console.log(data);
+  btns[1].click();
+  //so that when the screen first loads the 2nd btn
+  //- the weekly btn will be selected because JS will
+  //click on this button for us
+};
+
+//if we don't do this clearActivities() - each time we click a btn
+//a new set of data will be added to the existing data
+function clearActivities() {
+  const cards = document.querySelectorAll(".card");
+  console.log(cards);
+  //we will create this class and div in the
+  //displayCards function
+  cards.forEach((item) => item.remove());
+}
+
+//this is our main function and it is a LONG ONE
+const displayCards = async (clickedOption) => {
+  clearActivities();
+
+  //we want to go into our data Array and grab the activites/title
+  //b/c each object in the array is an activity -- work, play, study etc
+  data.forEach((item) => {
+    //console.log(item);
+    const name = item.title;
+
+    //BUT 1 problem is that Self Care has a space so
+    //we must add a dash because we can't have space
+    //in a class name - we do this:
+    const itemClass = name.toLowerCase().replace(" ", "-");
+    //console.log(itemClass); //gives us all work, play etc with self-care
+
+    const timeframeData = item.timeframes[clickedOption];
+    //we have 3 timeframes in our data array - daily, weekly, monthly
+    //and then we must use the string - clickedOption which we got above
+    //the daily, weekly, monthly BUT IT IS A STRING SO WE CAN'T USE dot
+    //notation so we must use []
+    //console.log(timeframeData); //gives us the current: 32, previous: 5 etc
+
+    const previousTimeframe = calcTimeframe(clickedOption);
+    //so here it must say yesterday or last week or last month
+    //BUT our objects in the array don't give us this so we must create it
+    //so we must create a function called calcTimeframe()
+    //and we pass it the clickedOption which is our daily, weekly, monthly
+    function calcTimeframe(option) {
+      if (option === "daily") {
+        return "Yesterday";
+      } else if (option === "weekly") {
+        return "Last Week";
+      } else if (option === "monthly") {
+        return "Last Month";
+      }
+    }
+    //console.log(previousTimeframe); //we get Yesterday, Last Week, Last Month
+
+    //now we will create a new section which does not currently exist in our HTML
+    const section = document.createElement("section");
+    //remember this is a .forEach loop so we will get 6
+
+    //then we add a class of card to it
+    section.classList.add("card", itemClass);
+    //this will be appended to HTML
+
+    const newSection = `<div class="card-${itemClass}">
+    <div class="card-bg">
+          <img src="images/icon-${itemClass}.svg" alt="" />
+        </div>
+        <div class="card-wrap" id="work">
+          <div class="title-line">
+            <div class="title">${name}</div>
+            <div class="svg">
+              <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+                  fill="#BBC0FF"
+                  fill-rule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
+          <div class="hours-line">
+            <h2 id="current">${timeframeData.current}hrs</h2>
+            <p id="previous">${previousTimeframe} - ${timeframeData.previous}hrs</p>
+          </div>
+        </div> </div>`;
+    section.innerHTML = newSection;
+    //console.log(section); //so we get all the sections when we click the btns
+    //but we don't want it here - we want to append it to renderCards()
+    container.append(section);
+    //above we get the container as this is where we want to append our
+    //newly created section HTML
+  });
+}; //FINALLY the END of our displayCards() function
+
+clickButtons(btns);
+loadData(); //Weekly is displaying when we load or refresh the page
